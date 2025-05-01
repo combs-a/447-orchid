0b48fe50ecb97bcd48d27a1977d9e1f617412cce
pashiav <pashiavang2022@gmail.com>
2025-04-30 21:30:24 -0500

    fix account view height

--- library/src/app/account/page.tsx
+++ library/src/app/account/page.tsx
@@ -7,8 +7,9 @@ import AccountDetails from "@/components/account/AccountDetails";
 import LoanHistory from "@/components/account/LoanHistory";
 import Reservations from "@/components/account/Reservations";
 import AccountSettings from "@/components/account/AccountSettings";
+import StaffPanel from "@/components/account/StaffPanel";
 
-type View = "details" | "history" | "reservations" | "settings";
+type View = "details" | "history" | "reservations" | "settings" | "staff";
 
 type Account = {
   account_id: number;
@@ -62,7 +63,7 @@ export default function AccountPage() {
   return (
     <>
       <Navigator user={user} setUser={setUser} />
-      <div className="flex h-screen text-black">
+      <div className="flex text-black h-[150vh] bg-white">
         {/* Sidebar */}
         <aside className="w-1/4 border-r p-4 bg-gray-50 space-y-4">
           <button
@@ -89,10 +90,20 @@ export default function AccountPage() {
           >
             Account Settings
           </button>
+
+          {user.account_type_id === 2 && (
+    <button
+      onClick={() => setView("staff")}
+      className="w-full text-left px-4 py-2 border rounded hover:bg-purple-100"
+    >
+      Management
+    </button>
+  )}
+
         </aside>
 
         {/* Main Content */}
-        <main className="w-3/4 p-6 border bg-white text-black">
+        <main className="w-3/4 p-6 border-t border-l bg-white text-black">
           {/* Details about account holder. */}
           {view === "details" && <AccountDetails user={user} />}
 
@@ -104,8 +115,12 @@ export default function AccountPage() {
           )}
 
           {/* Account Settings */}
-          {view === "settings" && <AccountSettings user={user} setUser={setUser} />} 
-          
+          {view === "settings" && (
+            <AccountSettings user={user} setUser={setUser} />
+          )}
+
+          {/* Staff Management */}
+          {view === "staff" && user.account_type_id === 2 && <StaffPanel />}
         </main>
       </div>
     </>
